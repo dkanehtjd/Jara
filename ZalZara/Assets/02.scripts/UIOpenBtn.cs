e1fcadace9e1ff8399ef47b651ecdb1a08f0521c
JeiLeee <72813810+JeiLeee@users.noreply.github.com>
2022-06-09 02:20:14 +0900

    v3.0.0
    프로젝트명 변경(jaljara->ZalZara)
    전체적인 UI 변경 및 교체
    Main씬 UI 시스템 일부 변경

---
+++ ZalZara/Assets/02.scripts/UIOpenBtn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOpenBtn : MonoBehaviour
+{
+    public GameObject gameObject;
+    public GameObject otherGameObject1;
+    public GameObject otherGameObject2;
+
+
+    public void GameObjectSetActive()
+    {
+        gameObject.SetActive(!gameObject.active);
+        otherGameObject1.SetActive(false);
+        otherGameObject2.SetActive(false);
+    }
+}
